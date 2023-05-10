e418acd744d0391a880d7c37a58ecb41def33fa7
mf.gareev <mf.gareev@tensor.ru>
2023-05-10 08:28:17 +0300

    Задание 3

--- Homework3/test_branch/task2.py
+++ Homework3/test_branch/task2.py
@@ -8,7 +8,7 @@ def get_list_info(lst):
     min_elem = min(lst)
     max_elem = max(lst)
     sum_list = sum(lst)
-    average = round(sum(lst)/len(lst),2)
+    average = round(sum(lst)/len(lst), 2)
     return min_elem, max_elem, sum_list, average
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
